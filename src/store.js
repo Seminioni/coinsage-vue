7f8d71a4f1943047a93a5aa95b8abb75a2a25ea8
Mikhail Semin <seminioni@gmail.com>
2018-10-14 16:09:29 +0300

    store: update index.js

--- src/store.js
+++ src/store.js
@@ -3,14 +3,24 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+/* eslint-disable */
+
 export default new Vuex.Store({
   state: {
-
+    favourites: [],
+    basket: [],
   },
   mutations: {
+    addFavourite(state, coin) {
+      state.favourites.push(coin)
+    },
+    removeFavourite(state, id) {
 
+    },
   },
   actions: {
-
+    addFavourite(context, payload) {
+      context.commit('addFavourite', payload)
+    }
   },
 });
